dd4bc0a52f2f6995c2cd10838e323041568ccd95
benwinmathew <benwinmathew@gmail.com>
2022-01-25 09:05:52 +0530

    formated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore,combineReducers,applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import contentReducer from './content/contentReducer'
-const reducer=combineReducers({contentReducer:contentReducer})
+const reducer = combineReducers({contentReducer: contentReducer})
 const store = createStore(reducer,applyMiddleware(thunk));
 
 export default store;
